b463b3b70279b3012158b427378d4748378645b6
SWING1993 <songguohua10446627@gmail.com>
2019-01-05 13:59:23 +0800

    update

--- main.py
+++
@@ -1,9 +0,0 @@
-from scrapy.cmdline import execute
-import os
-import sys
-
-
-#得到main文件的路径
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-#执行命令scrapy crawl jobbole
-execute(['scrapy', 'crawl', 'jobbole'])
