426f1d019abbdfa06ab5acd84cc177b4c0ee6e80
SWING1993 <songguohua10446627@gmail.com>
2019-01-05 14:16:12 +0800

    update

--- tutorial/tutorial/spiders/douyu.py
+++ tutorial/tutorial/spiders/douyu.py
@@ -4,8 +4,8 @@ import scrapy
 
 class DouyuSpider(scrapy.Spider):
     name = 'douyu'
-    allowed_domains = ['douyu.com']
-    start_urls = ['http://douyu.com/']
+    allowed_domains = ['www.douyu.com']
+    start_urls = ['http://www.douyu.com/']
 
     def parse(self, response):
         pass
